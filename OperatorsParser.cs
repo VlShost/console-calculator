859a011feca42feb32dda2174489309b8fd16d13
VlShost <vlshost9386@gmail.com>
2024-06-02 15:49:57 +0300

    Add OperatorsParser class
    OperatorsParser has ParseOperator method which checks if the given character corresponds to any defined value in the Operators enumeration using the Enum.IsDefined method.

---
+++ OperatorsParser.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator
+{
+    internal class OperatorsParser
+    {
+        public static Operators ParseOperator(char op)
+        {
+            if (Enum.IsDefined(typeof(Operators), (int)op))
+            {
+                return (Operators)op;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid operator", nameof(op));
+            }
+        }
+    }
+}
